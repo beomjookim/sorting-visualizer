28709ca091a20682420fd9a82c4f4b8d6ba51b33
beomjookim <globalman96@naver.com>
2022-03-17 23:27:20 +0900

    부가적인 기능까지 구현 완료

--- app/app.js
+++ app/app.js
@@ -1,16 +1,133 @@
 import * as sorting from './sorting-algorithms.js'
 
-const ARRAY_SIZE = 10;
+const barsElement = document.querySelector('.ani__animation');
+const genBtn = document.querySelector('.nav__gen-btn');
+const toggleBtn = document.querySelector('.nav__ctrl-toggle-btn');
+const stopBtn = document.querySelector('.nav__ctrl-stop-btn');
+const arrSizeSliderElement = document.querySelector('.nav__num-label');
+const aniSpeedSliderElement = document.querySelector('.nav__speed-label');
+const aniSliderElement = document.querySelector('.ani__slider');
+const enableNumSlider = document.querySelector('.nav__num-slider');
+const sortingAlgorithms = document.querySelector('.nav__algo-slt');
+
+let ARRAY_SIZE = 30;
+let ANIMATION_TIME = 10; // 최소 5 최대 100
+const BAR_WIDTH = Math.min((window.innerWidth-250)/ARRAY_SIZE, 50);
+
+let isPlaying = false;
+let isSorted = false;
+
+let barsData;
+let animationFrames;
+let interval;
 
 function genArray() {
-    let barsData = [];
-    let min = 10;
-    let max = window.innerHeight - 250;
-    const barWidth = Math.min((window.innerWidth-250)/ARRAY_SIZE, 50);
+    barsData = [];
+    const min = 10;
+    const max = window.innerHeight - 300;
 
     for (let i = 0; i < ARRAY_SIZE; i++) barsData.push(Math.floor(Math.random() * (max-min) + min));
 
-    return barsData.map((bar) => `<div class="bar" style="height: ${bar}px; width: ${barWidth}px"></div>`).join('');
+    isSorted = false;
+    render(barsData);
+    enableSizeControl();
+    setSlider(0);
 }
 
-document.querySelector('.wrapper__animation').innerHTML = genArray();
+genArray(); // 화면 처음 틀었을 때 새로운 어레이 렌더링
+
+// # of Bars 컨트롤 시 새로운 어레이 렌더링
+arrSizeSliderElement.addEventListener('input', e => {
+    if (!parseInt(aniSliderElement.value)){
+        ARRAY_SIZE = e.target.value;
+        genArray();
+    }
+    // 배열이 초기화된 상태가 아니면 안 움직이게!
+})
+
+// speed 컨트롤 시 해당 값 적용
+aniSpeedSliderElement.addEventListener('input', e => {
+    ANIMATION_TIME = 500/e.target.value;
+    if (isPlaying) {
+        pauseAnimation();
+        runAnimation();
+    }
+})
+
+aniSliderElement.addEventListener('input', e => {
+    if (!isSorted) sort(barsData);
+    if (isPlaying) pauseAnimation();
+    render(animationFrames[e.target.value].arr)
+})
+
+// sorting 시전: unsorted array 받아서 sorting 하면서 그 과정을 장면 별로 받음.
+function sort(barsData){
+    const result = sorting[sortingAlgorithms.value](barsData);
+    isSorted = true;
+    animationFrames = result.animationFrames;
+    aniSliderElement.setAttribute('max', animationFrames.length-1);
+};
+
+function runAnimation(){
+    isPlaying = true;
+    toggleBtn.setAttribute('class', 'mdi mdi-pause nav__ctrl-toggle-btn');
+    toggleBtn.innerHTML = ' PAUSE';
+    disableSizeControl();
+
+    let cur = aniSliderElement.value;
+    interval = setInterval(() => {
+        cur++;
+        render(animationFrames[cur].arr)
+        setSlider(cur);
+    }, ANIMATION_TIME)
+}
+
+function pauseAnimation(){
+    isPlaying = false;
+    toggleBtn.setAttribute('class', 'mdi mdi-play nav__ctrl-toggle-btn');
+    toggleBtn.innerHTML = ' PLAY';
+
+    clearInterval(interval);
+}
+
+function stopAnimation(){
+    if (interval) pauseAnimation();
+    setSlider(0);
+    enableSizeControl();
+    render(animationFrames[0].arr);
+    interval = null;
+}
+
+function setSlider(pos){
+    if (animationFrames && pos === animationFrames.length-1) {
+        toggler();
+        aniSliderElement.value = pos+1;
+        return;
+    }
+
+    aniSliderElement.value = pos;
+}
+
+function render(bars){
+    barsElement.innerHTML = bars.map((bar) => `<div class="bar" style="height: ${bar}px; width: ${BAR_WIDTH}px"></div>`).join('');
+}
+
+function toggler(){
+    if (isPlaying) pauseAnimation();
+    else {
+        if (!isSorted) sort(barsData); // 맨 처음 한번만 시뮬레이션
+        runAnimation();
+    }
+}
+
+function disableSizeControl(){
+    enableNumSlider.setAttribute('disabled', '')
+}
+
+function enableSizeControl(){
+    enableNumSlider.removeAttribute('disabled');
+}
+
+genBtn.onclick = genArray; // Generate Bars 눌렀을 때 새로운 어레이 렌더링
+toggleBtn.onclick = toggler; // toggle 버튼 눌렀을 때 버튼 class name 바꾸고 실행 혹은 일시 정지
+stopBtn.onclick = stopAnimation; // 정지 버튼 눌렀을 때 진행상황 초기화 및 화면 렌더링
---
+++ app/sorting-algorithms.js
@@ -0,0 +1,75 @@
+export function mergeSort(barsData) {
+    var animationFrames = [];
+    var sorted = [];
+    function addFrame({ compare = [], orderedIndex, start, mid, end }) {
+        animationFrames.push({
+            arr: barsData.slice(0),
+            compare,
+            orderedIndex,
+            start,
+            mid,
+            end,
+        })
+    }
+
+    function mergeSort(arr, start, end) {
+        if (start < end) {
+
+            let mid = parseInt((start + end) / 2);
+            mergeSort(arr, start, mid)
+            mergeSort(arr, mid + 1, end)
+
+            merge(arr, start, mid, end)
+        }
+    }
+
+    function merge(arr, start, mid, end) {
+
+
+        let arr2 = arr.slice(0);
+        let i = start, j = mid + 1;
+        let k = start;
+        while (i <= mid && j <= end) {
+            addFrame({ compare: [k, j], start, mid, end })
+            if (arr2[i] <= arr2[j]) {
+                addFrame({ compare: [k, j], orderedIndex: k, start, mid, end })
+                arr[k++] = arr2[i++]
+            }
+            else {
+                addFrame({ compare: [k, j], orderedIndex: j, start, mid, end })
+
+                arr.splice(j, 1)
+                arr.splice(k++, 0, arr2[j++])
+
+            }
+
+        }
+
+        while (i <= mid) {
+            addFrame({ compare: [k, j], start, mid, end })
+            arr[k++] = arr2[i++]
+        }
+
+        while (j <= end) {
+            addFrame({ compare: [k, j], start, mid, end })
+            arr.splice(j, 1)
+            arr.splice(k++, 0, arr2[j++])
+        }
+    }
+
+    addFrame({})
+    mergeSort(barsData, 0, barsData.length - 1)
+    addFrame({ start: 0, mid: (barsData.length - 1) / 2, end: barsData.length - 1 })
+
+    return {
+        animationFrames,
+        renderBars: (frameIndex) => {
+            let frame = animationFrames[frameIndex];
+            return frame.arr.map((bar, index) => {
+                let { arr, compare = [], orderedIndex, start, mid, end } = frame
+                return `
+            <div class="bar ${compare.includes(index) ? 'comparing' : ''} ${(start == 0 && end == arr.length - 1) && (compare[0] == undefined || index < compare[0]) ? 'sorted' : ''} ${orderedIndex == index ? 'ordered' : ''} ${start == index ? 'start' : ''} ${end == index ? 'end' : ''} ${mid == index ? 'mid' : ''} " style="height: ${bar}px; "></div>
+        `}).join("")
+        }
+    };
+}
